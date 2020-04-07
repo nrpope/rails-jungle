d981698e295e2057c263c9c9a73150936225c90f
Nick Pope <psydpope@gmail.com>
2020-04-07 02:17:36 +0000

    session controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,15 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  #Registration
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  #Login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
