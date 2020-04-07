70add25672353229b7fdd67ebea6d1799e84571f
Nick Pope <psydpope@gmail.com>
2020-04-07 01:51:26 +0000

    category save

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,5 +6,23 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+  
+  def create
+    @category = Category.new(cat_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category saved!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 
 end
